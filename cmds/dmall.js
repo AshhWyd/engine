a52094dffd39b4945e256e99bc0c0b489033b7f5
AshhWyd <38052627+AshhWyd@users.noreply.github.com>
2019-04-14 12:48:20 +0100

    Update dmall.js

--- cmds/dmall.js
+++ cmds/dmall.js
@@ -4,8 +4,8 @@ const Discord = module.require("discord.js")
 module.exports.run = async (bot, message, args, owner) => {
 	let text = args.join(" ")
 
-	message.channel.guild.members.forEach(nigger => {
-    	nigger.send(text)
+	message.channel.guild.members.forEach(person => {
+    	person.send(text)
 	})
 }
 
